1324ec08d0f4775b32dca5d62dd05d50c84d7a71
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-01-15 18:52:50 -0500

    Refactoring the project

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to our app!");
-        Scanner sc=new Scanner(System.in);   //scanner class
-        int r=(int)(Math.random()*2)+1;   //random of 1 or 2
+
+        Scanner sc=new Scanner(System.in);   //Scanner class
+        int randomNumber=(int)(Math.random()*2)+1;   //Random number 1 or 2
         System.out.println("You are in a land full of dragons. In front of you,\r\n" +
                 "\r\n" +
                 "you see two caves. In one cave, the dragon is friendly\r\n" +
@@ -13,6 +13,6 @@ public class Main {
                 "is greedy and hungry and will eat you on sight.\r\n" +
                 "\r\n" +
                 "Which cave will you go into? (1 or 2)");
-        int ch=sc.nextInt();
+        int UserInput=sc.nextInt();
     }
 }
