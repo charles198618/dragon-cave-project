2ba4f56b04e51845fe0ee2ad7c01b79b11f3da06
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-01-15 19:08:43 -0500

    Updating the code to add the conditional statement in the project

--- Main.java
+++ Main.java
@@ -14,5 +14,21 @@ public class Main {
                 "\r\n" +
                 "Which cave will you go into? (1 or 2)");
         int UserInput=sc.nextInt();
+        //int ch=sc.nextInt();   //user enters the choice of cave
+        if(UserInput==randomNumber) {   //if user's choice matches with the random value
+            System.out.println("You approach the cave...\r\n" +
+                    "\r\n" +
+                    "It is dark and spooky...\r\n" +
+                    "\r\n" +
+                    "A large dragon jumps out in front of you! He opens his jaws and...\r\n" +
+                    "\r\n" +
+                    "Gobbles you down in one bite!");
+        }
+        else if(UserInput>2){   //if the user enters an invalid choice
+            System.out.println("Wrong choice");
+        }
+        else {    //if the user's choice doesn't match with the random number
+            System.out.println("You approached the cave\n\nA friendly dragon appears...\n\nHe shares his treasure with you\n\nCongratulations you got your treasure!");
+        }
     }
 }
